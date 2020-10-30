d6bb457f20c75536bce643f0e235a7b7ad6acd22
13-3 <48124082+13-3@users.noreply.github.com>
2020-10-31 00:11:26 +0300

    Course information configured

--- app/src/main/java/com/example/onlinecourse/CourseInformation.java
+++ app/src/main/java/com/example/onlinecourse/CourseInformation.java
@@ -1,14 +1,69 @@
 package com.example.onlinecourse;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class CourseInformation extends AppCompatActivity {
+
+    RadioGroup languages_RadioGroup;
+    Button view_Button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_information);
+
+        languages_RadioGroup = findViewById(R.id.languages_RadioGroup);
+        view_Button = findViewById(R.id.view_Button);
+
+        view_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                int checkedId = languages_RadioGroup.getCheckedRadioButtonId();
+                findRadioButton(checkedId);
+
+
+            }
+        });
     }
+
+    public void findRadioButton(int checkedId)
+    {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this).setTitle("Language information");
+
+        switch (checkedId)
+        {
+            case R.id.java_RadioButton:
+                dialog.setMessage("This is java");
+                dialog.setNegativeButton("ok",null);
+                dialog.show();
+                break;
+
+            case R.id.kotlin_RadioButton:
+                dialog.setMessage("This is kotlin");
+                dialog.setNegativeButton("ok",null);
+                dialog.show();
+                break;
+
+            case R.id.android_RadioButton:
+                dialog.setMessage("This is android");
+                dialog.setNegativeButton("ok",null);
+                dialog.show();
+                break;
+
+            default:
+                Toast.makeText(this, "Please select a language", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+
+
 }
